68f613ee9be0ef9a39bece5f2f08b2a7fd6baca9
triplecheeseburger <hangyulkim94@gmail.com>
2022-08-18 18:06:49 +0900

    [feat] Router Header init

---
+++ Router/Router.h
@@ -0,0 +1,21 @@
+#ifndef MYLITTLEWEBSERV_ROUTER_H
+#define MYLITTLEWEBSERV_ROUTER_H
+
+#include "Config.h"
+#include "VirtualServer.h"
+#include "EventHandler.h"
+#include <algorithm>
+
+class Router {
+  private:
+    Config _config;
+    std::vector<VirtualServer> _virtual_servers;
+    EventHandler _event_handler;
+
+  public:
+    Router(const Config& config);
+    void start();
+};
+
+#endif
+
