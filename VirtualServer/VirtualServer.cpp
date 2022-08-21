3cd904b56f8cde2a0c177a154da58e1e97b36db0
triplecheeseburger <hangyulkim94@gmail.com>
2022-08-21 12:15:28 +0900

    [feat] VirtualServer implementing, callCgi have to be implemented

---
+++ VirtualServer/VirtualServer.cpp
@@ -0,0 +1,54 @@
+#include "VirtualServer.hpp"
+
+#include <sys/socket.h>
+
+VirtualServer::VirtualServer(int id, ServerInfo info) : _serverId(id), _serverInfo(info) {}
+
+void VirtualServer::start(EventHandler& eventHandler) {
+  std::vector<Event> event_list = eventHandler.getRoutedEvents(_serverId);
+  for (int i = 0; i < event_list.size(); i++) {
+    Event* event = &event_list[i];
+    switch (event->type) {
+      case CONNECTION_REQUEST:
+        eventHandler.addConnection(event->keventId);
+        break;
+      case HTTP_REQUEST_READABLE:
+        if (event->httpRequest->isCgi()) {
+          callCgi(event);
+        } else {
+          eventHandler.appendNewEventToChangeList(event->keventId, EVFILT_READ, EV_ADD | EV_ENABLE, event);
+        }
+        break;
+      case HTTP_RESPONSE_WRITABLE:
+        sendResponse(event->keventId(event->httpRequest));
+        eventHandler.appendNewEventToChangeList(event->keventId, EVFILT_READ, EV_ADD | EV_DISABLE, event);
+        eventHandler.appendNewEventToChangeList(event->keventId, EVFILT_WRITE, EV_ADD | EV_ENABLE, event);
+        if (!event->httpRequest->isKeepAlive()) {
+          eventHandler.removeConnection(event);
+        }
+        delete event;
+        break;
+      case CGI_RESPONSE_WRITABLE:
+        sendResponse(event->keventId, HttpResponse(event->cgiResponse));
+        if (!event->httpRequest->isKeepAlive()) {
+          eventHandler.removeConnection(event);
+        }
+        delete event;
+        break;
+      default:
+        break;
+    }
+  }
+}
+
+void VirtualServer::callCgi(Event* event) {
+
+}
+
+void sendResponse(int fd, HttpResponse& response) {
+  std::string response_str = response.getResponse();
+  if (send(fd, response_str.c_str(), response_str.size(), 0) == -1) {
+    throw "send() error!";
+  }
+}
+
--- VirtualServer/VirtualServer.hpp
+++ VirtualServer/VirtualServer.hpp
@@ -1,17 +1,21 @@
 #ifndef MYLITTLEWEBSERV_VIRTUALSERVER_HPP
 #define MYLITTLEWEBSERV_VIRTUALSERVER_HPP
 
-//have to include a header for struct ServerInfo
+#include "Config.hpp"
+#include "EventHandler.hpp"
+#include "HttpResponse.hpp"
 
 class VirtualServer {
-  private:
-    int _serverId;
-    ServerInfo _serverInfo;
+ private:
+  int        _serverId;
+  ServerInfo _serverInfo;
 
-  public:
-    VirtualServer(int id, ServerInfo info);
-    void start();
+  void callCgi(Event* event);
+  void sendResponse(int fd, HttpResponse& response);
+
+ public:
+  VirtualServer(int id, ServerInfo info);
+  void start(EventHandler& eventHandler);
 };
-
 
 #endif
